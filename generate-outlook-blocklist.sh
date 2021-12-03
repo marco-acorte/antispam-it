7f0de0fe798bc2b8af980aaa3c4b30460e8f518a
Marco Acorte <macorte@gmail.com>
2021-12-03 12:32:10 +0100

    add bash script to generate MS outlook blocklist

---
+++ generate-outlook-blocklist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while read p; do
+  echo "$p"
+done <antispam-it.txt
+
+while read p; do
+  echo "$p"
+done <antispam-emails.txt
---
+++ generate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./generate-spamassasin-blocklist.sh > spamassassin-blocklist.txt
+./generate-outlook-blocklist.sh > outlook-blocklist.txt
