b4bd17c1dacd3b7e1a46e4a84b2009cda3cb0c00
Marco Acorte <macorte@gmail.com>
2024-03-21 12:25:17 +0100

    Aggiungi generazione header.txt al file generate.sh

---
+++ generate-header.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# generate header.txt
+
+# 
+# Antispam-it | The Blocklist to trap a lot of rubbish ... hand-curated to fight spammers harassing poor mailboxes .it #spamrevenge
+# Strictly blocks advertisements networks, malwares, spams, statistics & trackers included in phishing/malware/spam 
+# campaign harassing poor italian (domain.it) mailboxes. Manually verified, and is updated regularly.
+# 
+# Last Update: Thu, 21 Mar 2024 05:46:20 UTC
+# 
+# Project website: https://github.com/marco-acorte/antispam-it/ 
+# Support the project with a donation:: https://www.buymeacoffee.com/marcoacorte 
+# 
+# This work is licensed under a GNU General Public License v3.0. 
+# ======================================================================================================
+
+echo "# " > header.txt
+echo "# Antispam-it | The Blocklist to trap a lot of rubbish ... hand-curated to fight spammers harassing poor mailboxes .it #spamrevenge" >> header.txt
+echo "# Strictly blocks advertisements networks, malwares, spams, statistics & trackers included in phishing/malware/spam " >> header.txt
+echo "# campaign harassing poor italian (domain.it) mailboxes. Manually verified, and is updated regularly." >> header.txt
+echo "# " >> header.txt
+echo "# Last Update: $(date -u)" >> header.txt
+echo "# " >> header.txt
+echo "# Project website: https://github.com/marco-acorte/antispam-it/ " >> header.txt
+echo "# " >> header.txt
+echo "# Support the project with a donation: https://www.buymeacoffee.com/marcoacorte" >> header.txt
+echo "# " >> header.txt
+echo "# This work is licensed under a GNU General Public License v3.0." >> header.txt
+echo "# ======================================================================================================" >> header.txt
+
+
--- generate.sh
+++ generate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# generate header.txt
+./generate-header.sh > header.txt
+
 ./generate-spamassasin-blocklist.sh > spamassassin-blocklist.txt
 ./generate-outlook-blocklist.sh > outlook-blocklist.txt
