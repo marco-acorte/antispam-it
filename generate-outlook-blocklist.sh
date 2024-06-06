5aa818ac528a10a777f81ee93df695662290585e
Marco Acorte <macorte@gmail.com>
2024-06-06 09:48:05 +0200

    Update blocklists

--- generate-outlook-blocklist.sh
+++ generate-outlook-blocklist.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# importing domains and emails from antispam-domains.txt
 while read p; do
   echo "$p"
 done <antispam-domains.txt
 
+# importing domains and emails from antispam-emails.txt
 while read p; do
   echo "$p"
 done <antispam-emails.txt
+
+# importing domains and emails from antispam-pec-it.txt
+while read p; do
+  echo "$p"
+done <antispam-pec-it.txt
--- generate-spamassasin-blocklist.sh
+++ generate-spamassasin-blocklist.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# importing domains and emails from antispam-domains.txt
 while read p; do
   echo "blacklist_from *@$p"
   echo "blacklist_from *.$p"
 done <antispam-domains.txt
 
+# importing emails from antispam-emails.txt
 while read p; do
   echo "blacklist_from $p"
 done <antispam-emails.txt
+
+# importing domains and emails from antispam-pec-it.txt
+while read p; do
+  echo "blacklist_from $p"
+done <antispam-pec-it.txt
