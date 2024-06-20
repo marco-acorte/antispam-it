dcffdaea66b74bbb4731d0e0cc348bd1f98e39e3
Marco Acorte <macorte@gmail.com>
2024-06-20 11:29:02 +0200

    Update blocklists ver. 2024.06.20.11.29.01

--- generate-hosts.sh
+++ generate-hosts.sh
@@ -2,18 +2,7 @@
 
 # generate hosts header
 echo "# Hosts file for blocking spam domains"
-echo "#"
-echo "# This hosts file is a combination of antispam-it.txt and the original hosts file from StevenBlack/hosts."
-echo "#"
-echo "# Last Update: $(date -u)"
-echo "#"
-echo "# Project website: https://github.com/marco-acorte/antispam-it/ "
-echo "#"
-echo "# Support the project with a donation: https://www.buymeacoffee.com/marcoacorte"
-echo "#"
-echo "# This work is licensed under a GNU General Public License v3.0."
-echo "# ======================================================================================================"
-echo "#"
+cat header.txt
 
 # generate hosts body
 cat antispam-domains.txt | while read line
